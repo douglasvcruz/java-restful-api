865cf25f338d4eb4e2e7f6d4426bdf64fcbf68b2
Douglas <cbdouglasvirissimo@hotmail.com>
2023-10-22 00:11:17 -0300

    feat: add model - Task and Person

---
+++ src/main/java/one/digitalinnovation/javarestfulapi/JavaRestfulApiApplication.java
@@ -0,0 +1,11 @@
+package one.digitalinnovation.javarestfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRestfulApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaRestfulApiApplication.class, args);
+    }
+}
---
+++ src/main/java/one/digitalinnovation/javarestfulapi/domain/model/Task.java
@@ -0,0 +1,55 @@
+package one.digitalinnovation.javarestfulapi.domain.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed;
+    @ManyToOne
+    private Person person;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+}
